eb54361b142b48c469098a38b4cf2b377047922c
Renos Bardis <59965737+BardisRenos@users.noreply.github.com>
2020-07-02 12:46:02 +0200

    Add files via upload

--- stop_words.py
+++ stop_words.py
@@ -19,18 +19,24 @@ given_sentence = df_amazon.iloc[2]['verified_reviews']
 # Setting the parser into English tokenizer
 parser = English()
 
+# Is an example of tokenization
 # Tokenize the text
-mytoken_text = parser(given_sentence)
+mytoken = parser(given_sentence)
 # Showing the tokenized words
-print(mytoken_text)
+print(mytoken)
+
+# Setting the list of punctuation
 # The punctuation characters
 punct = string.punctuation
 print(punct)
-# Lemmatize the words and convert into lower case words and strip them
-# from the empty fileds.
-my_tokens = [word.lemma_.lower().strip() for word in mytoken_text]
-# Removing the stop words and the punctuations.
-my_tokens = [word for word in my_tokens if word not in stop_words and word not in punct]
 
-print(my_tokens)
 
+def tokenizer_text(text):
+    # Lemmatize the words and convert into lower case words and strip them
+    # from the empty fileds.
+    mytoken_text = parser(text)
+    my_tokens = [word.lemma_.lower().strip() for word in mytoken_text]
+    # Removing the stop words and the punctuations.
+    my_tokens = [word for word in my_tokens if word not in stop_words and word not in punct]
+
+    return my_tokens
